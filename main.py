57361ddde2012b57d4b5e2b64740d3e7303628f6
praveen1082 <praveenpanta1082@gmail.com>
2022-03-08 09:53:45 +0545

    Fetching single value from database

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ DB_PASS = 'praveen'
 conn = database.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
 cur = conn.cursor()
-cur.execute("SELECT * FROM dividend;")
-print(cur.fetchall())
+cur.execute("SELECT * FROM dividend;", (1,))
+# print(cur.fetchall())
+print(cur.fetchone())
 conn.close()
 
 
