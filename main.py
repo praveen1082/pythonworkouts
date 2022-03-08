226ab71e345c15deb4941785bfc6c110028e8d08
praveen1082 <praveenpanta1082@gmail.com>
2022-03-08 10:22:56 +0545

    Some workouts wwith database records and methods of displaying them

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ DB_PASS = 'praveen'
 try:
     conn = database.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
     cur = conn.cursor()
+    create_script = '''CREATE TABLE IF NOT EXISTS employee(
+                        id int PRIMARY KEY,
+                        name varchar(40) NOT NULL,
+                        salary int,
+                        dept_id varchar(30)
+    )'''
+    cur.execute(create_script)
+    # insert_script = 'INSERT INTO employee (id,name,salary,dept_id) VALUES (%s,%s,%s,%s)'
+    # insert_values = [(1, 'praveen', 20000, 'd2'), (2, 'nabin', 50000, 'd1'), (3, 'sawbeen', 10000, 'd1')]
+    # for record in insert_values:
+    #     cur.execute(insert_script, record)
+    cur.execute('Select * from employee')
+    for record in cur.fetchall():
+        print(record)
+    conn.commit()
 except Exception as error:
     print(error)
 finally:
